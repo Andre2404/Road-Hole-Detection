319998cf93bf4e5fbd81967c8b8ab6ef02e2fff1
dewisiti28 <dewisitijamilah@upi.edu>
2026-01-01 16:46:16 +0700

    hapus

--- menyoba.js
+++
@@ -1,9 +0,0 @@
-// Fungsi untuk menghitung rata-rata angka dalam array
-const hitungRataRata = (data) => {
-  const total = data.reduce((acc, curr) => acc + curr, 0);
-  return total / data.length;
-};
-
-// Cara pakai:
-const suhuMotor = [45, 47, 44, 48, 50];
-console.log("Rata-rata suhu:", hitungRataRata(suhuMotor));
